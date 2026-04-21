4e3ea043eed01679e67c1f34313cb3ed24cd5799
john-saccomando <john.saccomando@maine.edu>
2026-04-21 19:06:03 -0400

    I dunno if I even merge this one tbh 😢😢😢

--- Modules/Ball.sv
+++ Modules/Ball.sv
@@ -1,8 +1,7 @@
 module ball(
-	//output ballx,
-	//output bally
-	input pixel_clk
-	 
+	output [9:0] ballx,
+	output [9:0] bally,
+	input clk
 );
 
 
@@ -44,34 +43,40 @@ module ball(
 
 parameter [11:0] STARTX	= 12'd320;
 parameter [11:0] STARTY	= 12'd240;
-parameter [11:0] LEFT 	= 12'd90; //I am sure this is the best way to implement this and this will cause no problems in the future
-parameter [11:0] RIGHT	= 12'd270;
-parameter [11:0] UP		= 12'd0;
-parameter [11:0] DOWN		= 12'd180;
+//parameter [11:0] LEFT 	= 12'd90; //I am sure this is the best way to implement this and this will cause no problems in the future
+//parameter [11:0] RIGHT	= 12'd-90;
+//parameter [11:0] UP		= 12'd90;
+//parameter [11:0] DOWN		= (12'd90)*-1;
 parameter [11:0] LEFTPADDLE =12'd200; //wrong rn but will be the x location where the left paddle is
 parameter [11:0] RIGHTPADDLE =12'd200; //" 														  right paddle is
 
 
 
-reg [11:0] ballx;
-reg [11:0] bally;
-reg [11:0] balldir;
-reg [11:0] ballspeed;
+//reg [11:0] ballx;
+//reg [11:0] bally;
+reg signed [11:0] ball_speed_x;
+reg signed [11:0] ball_speed_y;
+reg [32:0] ballspeed;
+reg signed [2:0] ball_dir_x;
+reg signed [2:0] ball_dir_y;
 
 initial begin //at the start of the program make the ball in the center
  ballx = STARTX;
  bally = STARTY;
- balldir = LEFT;
+ ball_dir_x = -1;
+ ball_speed_x =10;
+ ball_speed_y =10;
 end
  
-always_ff @(posedge pixel_clk) begin
+always_ff @(posedge clk) begin
 
 ballspeed = ballspeed +1;
 		
-	if(ballspeed >1000) begin //after 1000 cycles of the pixel clock actually move the ball	
+	if(ballspeed >500000) begin //after 1000 cycles of the pixel clock actually move the ball	
 		ballspeed = 0;
-		bally= floor(cos(balldir)) + bally; //ball's movement in the y direction
-		ballx = floor(sin(balldir)) + ballx; //in the x direction
+		//balldir = 0;
+		bally = ((ball_speed_y*ball_dir_y) + bally); //ball's movement in the y direction
+		ballx = ((ball_speed_x*ball_dir_x) + ballx); //in the x direction
 	end
 	if(ballx == LEFTPADDLE) begin //on a per paddle basis is way easier than doing both at the same time 
 	
@@ -81,15 +86,24 @@ ballspeed = ballspeed +1;
 			//eg dead center = balldir +180 ? you might need to do something fancy with multiplication?
 	end
 	
-	if((ballx == 640) || (ballx == 0)) begin //what do we do if we touch a wall?
-		//freak the fuck out
-		if(balldir>=180) //if ball is going down
-			balldir = balldir-180;
-		if(balldir<180) //if ball is going up make it go down
-			balldir = balldir+180;
-		
+	if(((ballx+10) >= 600)) begin //what do we do if we touch a wall?
+		//freak the fuck out //if ball is going right make it go left
+			ball_dir_x = -1;
+			ballx = 550;
+
 	end
 	
+	if((ballx =0)) begin //if ball is going left make it go right
+			ball_dir_x = 1; 
+			ballx = 10;
+	end
+	
+	if(((bally+10) >= 270) || (bally = 0)) begin
+			if(ball_dir_y>=0) //if ball is going up
+			ball_dir_y = -ball_dir_y;
+		if(ball_dir_y<0) //if ball is going up make it go down
+			ball_dir_y = -ball_dir_y;
+	end
 end
 	
 endmodule
