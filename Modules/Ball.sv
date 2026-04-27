39007d394b4524d5ae0b71c125694789f75c3559
Bayley Baldridge <bayley.baldridge@maine.edu>
2026-04-27 15:37:02 -0400

    Cleaned up the code

--- Modules/Ball.sv
+++ Modules/Ball.sv
@@ -5,53 +5,12 @@ module ball(
 );
 
 
-
-//to do write sick code that does the math to move a ball from one side of the 
-
-
-// create small rectangle
-// have it move on the clock at a reasonable speed
-// check if it is on the same x level as the paddle
-// output its y level if its on the x level as any paddle so it knows to bounce off
-		//maybe put output here and move to a new module for collision stuff
-//be able to take in that its collidied and assign it new angle
-
-
-
-//state machine concept
-
-// exist
-// move to left or right
-// go back to moving
-// check if collided
-// change direction
-// return to move left or right
-
-
-//what is a box?
-/*
-	input [9:0] X_pix,
-	input [9:0] Y_pix,
-	input [9:0] box_width,
-	input [9:0] box_height,
-	input [9:0] box_x_location,
-	input [9:0] box_y_location,
-	input pixel_clk,
-	output box
-*/
-
-
 parameter [11:0] STARTX	= 12'd320;
 parameter [11:0] STARTY	= 12'd240;
-//parameter [11:0] LEFT 	= 12'd90; //I am sure this is the best way to implement this and this will cause no problems in the future
-//parameter [11:0] RIGHT	= 12'd-90;
-//parameter [11:0] UP		= 12'd90;
-//parameter [11:0] DOWN		= (12'd90)*-1;
+
 parameter [11:0] LEFTPADDLE =12'd200; //wrong rn but will be the x location where the left paddle is
 parameter [11:0] RIGHTPADDLE =12'd200; //" 														  right paddle is
 
-//reg [11:0] ballx;
-//reg [11:0] bally;
 reg signed [11:0] ball_speed_x;
 reg signed [11:0] ball_speed_y;
 reg [32:0] ballspeed;
@@ -67,13 +26,11 @@ end
  
 always_ff @(posedge clk) begin
 
-ballspeed = ballspeed +1;
+	ballspeed = ballspeed +1;
 		
 	if(ballspeed >300000) begin
 		ballspeed = 0;
-		
-		
-		//ballx = 5*(ball_dir_x) + ballx;
+
 		if(ball_dir_x==1) begin
 			ballx = (ballx+1);
 			
@@ -90,8 +47,7 @@ ballspeed = ballspeed +1;
 			end
 		end
 		
-		
-				if(ball_dir_y==1) begin
+		if(ball_dir_y==1) begin
 			bally = (bally+1);
 			
 			if(bally > 465) begin
@@ -107,11 +63,8 @@ ballspeed = ballspeed +1;
 			end
 		end
 		
-		
-		end
-		
+	end
 	
-
 end
 	
 endmodule
--- Modules/Paddle.sv
+++ Modules/Paddle.sv
@@ -6,42 +6,6 @@ module paddle(
 	input clk
 );
 
-//to do write sick code that does the math to move a ball from one side of the 
-
-
-// create small rectangle
-// have it move on the clock at a reasonable speed
-// check if it is on the same x level as the paddle
-// output its y level if its on the x level as any paddle so it knows to bounce off
-		//maybe put output here and move to a new module for collision stuff
-//be able to take in that its collidied and assign it new angle
-
-
-
-//state machine concept
-
-// exist
-// move to left or right
-// go back to moving
-// check if collided
-// change direction
-// return to move left or right
-
-
-//what is a box?
-/*
-	input [9:0] X_pix,
-	input [9:0] Y_pix,
-	input [9:0] box_width,
-	input [9:0] box_height,
-	input [9:0] box_x_location,
-	input [9:0] box_y_location,
-	input pixel_clk,
-	output box
-*/
-
-//reg [11:0] ballx;
-//reg [11:0] bally;
 parameter [11:0] SPEED	= 12'd5;
 parameter [9:0] START_Y = 10'd220;
 parameter [9:0] PADDLE_TOP_BOUND	= 12'd5;
@@ -71,43 +35,7 @@ always_ff @(posedge clk) begin
 		if( (u_input[1:1] == 1) && (paddle_y > PADDLE_TOP_BOUND) ) begin
 			paddle_y = paddle_y - SPEED;
 		end
-		
-				
-		/*
-		//ballx = 5*(ball_dir_x) + ballx;
-		if(ball_dir_x==1) begin
-			ballx = (ballx+5);
-			
-			if(ballx > 600) begin
-				ball_dir_x = -1;
-			end
-		end
-		
-		if(ball_dir_x==-1) begin
-			ballx = (ballx-5);
-			
-			if(ballx < 50) begin
-				ball_dir_x = 1;
-			end
-		end
-		
-		
-				if(ball_dir_y==1) begin
-			bally = (bally+5);
-			
-			if(bally > 450) begin
-				ball_dir_y = -1;
-			end
-		end
-		
-		if(ball_dir_y==-1) begin
-			bally = (bally-5);
-			
-			if(bally < 50) begin
-				ball_dir_y = 1;
-			end
-		end
-		*/
+
 		end
 end
 	
